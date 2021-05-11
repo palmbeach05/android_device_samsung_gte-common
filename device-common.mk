f977064df275f238fb68aca3504644d68ae41c4a
palmbeach05 <davmcleod87@aol.com>
2021-05-10 21:52:10 -0400

    gte-common: Migrate to DEVICE_PATH
    * Replace the LOCAL_PATH instances with a variable not prone to
      getting stomped
    * Replace all expanded values with the variable

--- device-common.mk
+++ device-common.mk
@@ -21,13 +21,13 @@ $(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 # Inherit from vendor
 $(call inherit-product-if-exists, vendor/samsung/gte-common/gte-common-vendor.mk)
 
-LOCAL_PATH := device/samsung/gte-common
+DEVICE_PATH := device/samsung/gte-common
 
 # System properties
--include $(LOCAL_PATH)/system_prop.mk
+-include $(DEVICE_PATH)/system_prop.mk
 
 # Common overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
 # Camera
 PRODUCT_PACKAGES += Camera2
@@ -38,7 +38,7 @@ PRODUCT_PACKAGES += \
 
 # Media (camera) configuration files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+	$(DEVICE_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
