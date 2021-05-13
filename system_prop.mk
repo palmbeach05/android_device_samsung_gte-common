e2372a663bdd74e9aff119fb48ee728698d63667
palmbeach05 <davmcleod87@aol.com>
2021-05-13 12:12:58 -0400

    msm8916-common: Commonize OpenGLES and Volume

--- system_prop.mk
+++ system_prop.mk
@@ -2,17 +2,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	qemu.hw.mainkeys=0
 
-# OpenGLES
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196608
-
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.radio.snapshot_enabled=1 \
 	persist.radio.snapshot_timer=22 \
 	persist.radio.lte_vrte_ltd=1
-
-# Volume
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.vc_call_vol_steps=15 \
-	ro.config.media_vol_steps=30
